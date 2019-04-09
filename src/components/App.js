8893ed004a44c339ab8df98f2f6fc7c833f62a87
Refayat Haque <refayathaque@gmail.com>
2019-04-08 22:08:13 -0400

    fsdfasfsf

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import SearchBar from 'components/SearchBar'
 import youtube from 'api/youtube'
 import VideoList from 'components/VideoList'
+import VideoDetail from 'components/VideoDetail'
 
 class App extends Component {
 
   constructor(props) {
     super(props)
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
     this.onTermSubmit = this.onTermSubmit.bind(this)
   }
 
@@ -23,11 +27,27 @@ class App extends Component {
     console.log(this.state.videos)
   }
 
+  onVideoSelect = (video) => {
+    console.log('From the App Component - ',video)
+    this.setState({ selectedVideo: video })
+  }
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos} onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import 'components/VideoItem.css'
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <div className="video-item item">
-      <img className="ui image" src={video.snippet.thumbnails.medium.url}/>
+    <div onClick={() => onVideoSelect(video)} className="video-item item">
+      <img
+        alt={video.snippet.title} className="ui image" src={video.snippet.thumbnails.medium.url}
+      />
       <div className="content">
         <div className="header">
           {video.snippet.title}
